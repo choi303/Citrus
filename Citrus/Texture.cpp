af94c385e5653b016d55f268d518d025384817e3
Choi <ridvansu@hotmail.com>
2021-08-23 18:15:57 +0300

    HDR file support added.

--- Citrus/Texture.cpp
+++ Citrus/Texture.cpp
@@ -59,6 +59,33 @@ Texture::Texture(ID3D11Device* pDevice, ID3D11DeviceContext* pContext, const std
 		hr = pDevice->CreateShaderResourceView(m_resource.Get(), &srvDesc, &m_view);
 		if (FAILED(hr)) { Error::Log(hr, "Failed to create shader resource view"); }
 	}
+	else if (extension == "hdr" || extension == "HDR")
+	{
+		HRESULT hr = E_FAIL;
+		DirectX::ScratchImage pScratch;
+		DirectX::TexMetadata pData;
+		hr = DirectX::LoadFromHDRFile(Converter::ToWide(filepath).c_str(), &pData, pScratch);
+		if (FAILED(hr)) { Error::Log(hr, "Failed to load hdr texture from file"); }
+
+		const DirectX::Image* pImage = pScratch.GetImage(0, 0, 0);
+		assert(pImage);
+
+		CD3D11_TEXTURE2D_DESC textureDesc(pImage->format, static_cast<UINT>(pImage->width), static_cast<UINT>(pImage->height));
+		textureDesc.MipLevels = 1;
+		textureDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET;
+		textureDesc.MiscFlags = D3D11_RESOURCE_MISC_GENERATE_MIPS;
+		ID3D11Texture2D* p2DTexture = nullptr;
+		D3D11_SUBRESOURCE_DATA subResourceData{};
+		subResourceData.pSysMem = pImage->pixels;
+		subResourceData.SysMemPitch = static_cast<UINT>(pImage->rowPitch);
+		hr = pDevice->CreateTexture2D(&textureDesc, &subResourceData, &p2DTexture);
+		if (FAILED(hr)) { Error::Log(hr, "Failed to create texture2d"); }
+
+		m_resource = static_cast<ID3D11Texture2D*>(p2DTexture);
+		CD3D11_SHADER_RESOURCE_VIEW_DESC srvDesc(D3D11_SRV_DIMENSION_TEXTURE2D, textureDesc.Format);
+		hr = pDevice->CreateShaderResourceView(m_resource.Get(), &srvDesc, &m_view);
+		if (FAILED(hr)) { Error::Log(hr, "Failed to create shader resource view"); }
+	}
 	else
 	{
 		HRESULT hr = E_FAIL;
